55924ad0eb5a07ae6020e0d0f35f3a626e2e8ba3
deanamiel <deanamiel1@gmail.com>
2022-03-23 00:12:55 -0400

    Added price-watcher support for eth

--- scripts/price-watcher.js
+++ scripts/price-watcher.js
@@ -9,8 +9,8 @@ module.exports = async done => {
     const oracle = await Oracle.deployed();
 
     while(true) {
-        const response = await CoinGeckoClient.coins.fetch('bitcoin', {});
-        let currentPrice = parseFloat(response.data.market_data.current_price.usd);
+        const responseBTC = await CoinGeckoClient.coins.fetch('bitcoin', {});
+        let currentPrice = parseFloat(responseBTC.data.market_data.current_price.usd);
         currentPrice = parseInt(currentPrice * 100);
 
         await oracle.updateData(
@@ -20,6 +20,18 @@ module.exports = async done => {
         );
 
         console.log(`New price for BTC/USD ${currentPrice} updated on-chain`);
+
+        const responseETH = await CoinGeckoClient.coins.fetch('ethereum', {});
+        currentPrice = parseFloat(responseETH.data.market_data.current_price.usd);
+        currentPrice = parseInt(currentPrice * 100);
+
+        await oracle.updateData(
+            web3.utils.soliditySha3('ETH/USD'),
+            currentPrice,
+            {from: reporter}
+        );
+
+        console.log(`New price for ETH/USD ${currentPrice} updated on-chain`);
         await new Promise(resolve => setTimeout(resolve, POLL_INTERVAL));
     }
 
